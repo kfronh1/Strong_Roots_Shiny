7be820634d7d2e2d49eab912f88056832fd039e5
Zoe Duerksen-Salm <zduerksen-salm@umail.ucsb.edu>
2021-02-07 08:25:31 -0800

    Bones of "project impact"

--- corridor_df.R
+++ corridor_df.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 library(janitor)
-
+library(lubridate)
 
 corridor <- read_csv("Corridor.csv") %>%
   clean_names()
+
+projects_all <- read_csv("project_df.csv") %>%
+  clean_names() %>%
+  mutate(date = lubridate::mdy(date))
+
